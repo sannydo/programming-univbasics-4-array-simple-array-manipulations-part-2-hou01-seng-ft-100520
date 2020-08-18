64380918eb46ddd1acd1fe1cb6bad91ee6361a39
Sanny Do <sanny.do94@gmail.com>
2020-08-18 20:15:41 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,8 +14,8 @@ def using_flatten(array)
   array.flatten
 end
 
-def using_delete(array)
-  array.delete
+def using_delete(array,string)
+  array.delete(string)
 end
 
 def using_delete_at(array, integer)
